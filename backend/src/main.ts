99a368b3a0133db1415ac355393d266a9e432bd8
xtvlw <bernardopereira01@pm.me>
2022-11-15 15:24:46 -0300

    adding connection in database with api, next insert data.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,26 @@ import source from './database/index'
 const server = express()
 const port: number = 4000
 
+source.initialize()
+    .then(() => console.log("database online"))
+
+
 server.get('/', (req: Request, res: Response) => {
     res.send({
         status: 200
     })
 })
 
+server.get("/newUser", async (req: Request, res: Response) => {
+    
+    res.send({
+        status: 200,
+        opetation: null
+    })
+})
+
+
+
 server.listen(port, async () => {
     console.log("server is online");
-    source.initialize()
-        .then(() => console.log("database online"))
 })
